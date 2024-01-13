d769d0e401e04b2009d8e243b907acda88fb8767
Maycon <csa.devmaycon@gmail.com>
2024-01-13 10:24:12 -0300

    Desafio concluido

---
+++ Models/Smartphone.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
+
+
+        public Smartphone(string numero , string modelo, string imei, int memoria)
+        {
+            this.Numero = numero;
+            this.Modelo = modelo;
+            this.IMEI = imei;
+            this.Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo:"Modelo 1", imei:"1111111111", memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine('\n');
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "2222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine('\n');
+
